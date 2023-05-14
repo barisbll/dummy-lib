e022778c2adcd992382f6cb7393ae0373c3ea5c8
Baris Balli <barisbll.dev@gmail.com>
2023-05-14 19:26:58 +0200

    main: initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param num Number
+ * @returns Number
+ * @description Returns the boolean value if the number is even or not
+ */
+export const isDouble = (num: number): Boolean => {
+    return num % 2 === 0;
+};
