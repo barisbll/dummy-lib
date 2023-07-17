5e169c3c6a4f576a7a0961b5628a2e5957d7c81f
Baris Balli <barisbll.dev@gmail.com>
2023-07-17 12:40:34 +0300

    isEven added back again

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,13 @@
  * isOdd(2) // false
  * */
 export const isOdd = (n: number) => n % 2 === 1;
+
+/**
+ * @description
+ * @param {number} n
+ * @returns {boolean}
+ * @example
+ * isEven(1) // false
+ * isEven(2) // true
+ * */
+export const isEven = (n: number) => n % 2 === 0;
