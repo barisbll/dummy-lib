dc9217c66f99bc2c5e03f7a8f0a7d2e7d55e8367
Baris Balli <barisbll.dev@gmail.com>
2023-07-17 12:08:14 +0300

    tsup added

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @description
+ * @param {number} n
+ * @returns {boolean}
+ * @example
+ * isOdd(1) // true
+ * isOdd(2) // false
+ * */
+export const isOdd = (n: number) => n % 2 === 1;
+
+/**
+ * @description
+ * @param {number} n
+ * @returns {boolean}
+ * @example
+ * isEven(1) // false
+ * isEven(2) // true
+ * */
+export const isEven = (n: number) => n % 2 === 0;
